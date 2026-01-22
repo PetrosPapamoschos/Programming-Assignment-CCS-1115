ee364b96ccd94c09c20da0d610ca4f069f8ad761
Stergios P <stepetalo@protonmail.com>
2026-01-22 18:07:12 +0200

    sumOfHouse add

--- FourHouses.java
+++ FourHouses.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class FourHouses{
     public static void main(String[] args) {
         
 
     }
+
+    public static int sumOfHouse(int[] house){
+        int sum = 0;
+        for(int i = 0; i < house.length; i++){
+            sum += house[i];
+        }
+        return sum;
+    }
+
 }
